9500eace09caefbe8b602189ec9593990c9882e1
esteinig <eike.steinig@unimelb.edu.au>
2024-03-29 16:02:31 +1100

    chore(dev): recent update

--- app/src/routes/cerebro/+page.svelte
+++ app/src/routes/cerebro/+page.svelte
@@ -3,6 +3,9 @@
 	import CerebroRelease from "$lib/components/dashboard/CerebroRelease.svelte";
 	import TeamActivity from "$lib/components/dashboard/TeamActivity.svelte";
     import UserProfileSummary from "$lib/components/dashboard/UserProfileSummary.svelte";
+	import { ListBox, ListBoxItem } from "@skeletonlabs/skeleton";
+
+    let selectedView: string = "samples";
 </script>
 
 
@@ -11,9 +14,22 @@
     
     <div class="grid sm:grid-cols-1 md:grid-cols-8 gap-8 h-full">
         <div class="md:col-span-2">
-            <div class="grid grid-rows-1 md:grid-rows-1 gap-y-8">
+            <div class="grid grid-rows-2 md:grid-rows-2 gap-16">
                 <div class="row-span-1">
                     <UserProfileSummary></UserProfileSummary>
+                </div>
+                <div class="row-span-1">
+                    <p class="opacity-60">Navigation</p>
+                    <div class="card border p-4 border-primary-500">
+                        <ListBox class="">
+                            <ListBoxItem bind:group={selectedView} name="medium" value="samples" active='variant-soft' rounded='rounded-token'>Samples & Clinical Reports</ListBoxItem>
+                            <ListBoxItem bind:group={selectedView} name="medium" value="runs", active='variant-soft' rounded='rounded-token'>Sequence Runs & Data </ListBoxItem>
+                            <ListBoxItem bind:group={selectedView} name="medium" value="taxdb", active='variant-soft' rounded='rounded-token'>Taxonomy & Databases</ListBoxItem>
+                            <ListBoxItem bind:group={selectedView} name="medium" value="qa", active='variant-soft' rounded='rounded-token'>Quality Assurance</ListBoxItem>
+                            <ListBoxItem bind:group={selectedView} name="medium" value="tools", active='variant-soft' rounded='rounded-token'>Other Tools</ListBoxItem>
+                        
+                        </ListBox>
+                    </div>                    
                 </div>
             </div>
         </div>
