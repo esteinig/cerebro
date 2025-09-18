ab344f4792d431a971916532f4165084df70d15e
esteinig <eike.steinig@unimelb.edu.au>
2025-09-19 09:33:25 +1000

    chore(dev): disable pipeline deployment button

--- app/src/lib/components/dashboard/PipelineDeployment.svelte
+++ app/src/lib/components/dashboard/PipelineDeployment.svelte
@@ -14,7 +14,7 @@
         <section>
             <div class="p-4">
                 <ListBox>
-                    <ListBoxItem bind:group={selectedView} name="medium" value="watchers" active='variant-soft' rounded='rounded-token' on:click={() => goto(`./cerebro/production/watchers/team=${selectedTeam}&watcher=0`)}>
+                    <ListBoxItem bind:group={selectedView} name="medium" value="watchers" active='variant-soft' rounded='rounded-token' on:click={() => goto(`./cerebro/production/watchers/team=${selectedTeam}&watcher=0`)} disabled>
                         <svelte:fragment slot="lead">
                             <div class="w-4 h-4">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-satellite-dish"><path d="M4 10a7.31 7.31 0 0 0 10 10Z"/><path d="m9 15 3-3"/><path d="M17 13a6 6 0 0 0-6-6"/><path d="M21 13A10 10 0 0 0 11 3"/></svg>
