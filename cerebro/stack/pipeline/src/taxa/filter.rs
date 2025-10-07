5eab9d9b22d15fcca0b79d8dbd0ae86ef18dfae0
esteinig <eike.steinig@unimelb.edu.au>
2025-10-08 09:34:56 +1100

    chore(dev): prefetch test r2

--- cerebro/stack/pipeline/src/taxa/filter.rs
+++ cerebro/stack/pipeline/src/taxa/filter.rs
@@ -376,11 +376,11 @@ impl LineageFilterConfig {
         Self {
             lineages: vec!["d__Viruses".to_string()],
             tags: vec!["DNA".to_string(), "RNA".to_string()],
-            min_alignment_tools: Some(1),
-            min_alignment_rpm: Some(0.0),
-            min_alignment_regions: Some(2),
-            min_alignment_regions_coverage: Some(20.0),
-            min_kmer_tools: Some(1),
+            min_alignment_tools: None,
+            min_alignment_rpm: None,
+            min_alignment_regions: None,
+            min_alignment_regions_coverage: None,
+            min_kmer_tools: Some(3),
             min_kmer_rpm: Some(0.0),
             min_assembly_tools: None
         }
