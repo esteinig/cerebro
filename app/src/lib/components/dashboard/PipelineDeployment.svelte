798a5ebfc235e56ee4b365edb427a365695a57c8
esteinig <eike.steinig@unimelb.edu.au>
2026-03-17 15:59:45 +1100

    chore: fix dashboard link

--- app/src/lib/components/dashboard/PipelineDeployment.svelte
+++ app/src/lib/components/dashboard/PipelineDeployment.svelte
@@ -8,19 +8,19 @@
 </script>
 
 <div>
-    <p class="opacity-60">Production operations</p>
+    <p class="opacity-60">Standard operations</p>
     <div class="card w-full border border-primary-500">
         <section>
             <div class="p-4">
                 <ListBox>
                     <a href="/cerebro/data/samples/team=0&db=0&project=0">
-                        <ListBoxItem bind:group={selectedView} name="medium" value="runs", active='variant-soft' rounded='rounded-token'>
+                        <ListBoxItem bind:group={selectedView} name="medium" value="samples" active='variant-soft' rounded='rounded-token'>
                             <svelte:fragment slot="lead">
                                 <div class="w-4 h-4">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-test-tubes"><path d="M9 2v17.5A2.5 2.5 0 0 1 6.5 22v0A2.5 2.5 0 0 1 4 19.5V2"/><path d="M20 2v17.5a2.5 2.5 0 0 1-2.5 2.5v0a2.5 2.5 0 0 1-2.5-2.5V2"/><path d="M3 2h7"/><path d="M14 2h7"/><path d="M9 16H4"/><path d="M20 16h-5"/></svg>
                                 </div>
-                            </svelte:fragment> 
-                            Data Interface 
+                            </svelte:fragment>
+                            Data Interface
                         </ListBoxItem>
                     </a>
                     <a href="/cerebro/production/reports">
